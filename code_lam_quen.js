9e5278d2d7a5f5bf55fcc393a938a0e792f5cbb8
Nguyenhuy18062006 <huyn65830@gmail.com>
2025-04-01 21:12:49 +0700

    code 4.1/2025

--- code_lam_quen.js
+++ code_lam_quen.js
@@ -1,21 +1,25 @@
-console.log("Xin chào VSCode!");
-let tên = 'mosh';
-console.log(tên);
-let số = 0.1;
-số = 1;
-console.log(số);
-for(var i=0;i<=a;i++){
-    console.log(i);
-}
+// console.log("Xin chào VSCode!");
+// let tên = "mosh";
+// console.log(tên);
+// let số = 0.1;
+// số = 1;
+// console.log(số);
+// for (var i = 0; i <= a; i++) {
+//   console.log(i);
+// }
 // Ví dụ về 3 kiểu dữ liệu;
 //var
-var tuổi = 18;
-var tuổi= 12;
-var check = false;
-console.log(tuổi);l
-//let
-let namsinh = 2006;
-let namsinh =2005;
-//const 
-const số2 = 12;
-console.log(tuổi, namsinh , số2);
+// var tuổi = 18;
+// var tuổi= 12;
+// var check = false;
+// console.log(tuổi);l
+// //let
+// let namsinh = 2006;
+// let namsinh =2005;
+// //const
+// const số2 = 12;
+// console.log(tuổi, namsinh , số2);
+alert("Xin chaò");
+let userName = prompt("Nhập tên của bạn vào: ");
+console.log("Tên của bạn là: " + userName);
+alert("Chào mừng: " + userName);
